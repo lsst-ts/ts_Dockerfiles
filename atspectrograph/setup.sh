1c83acde948d8f1c9501ec0587310752f256cd36
Tiago Ribeiro <tiago.astro@gmail.com>
2019-02-20 10:44:15 -0700

    Test with rpm install of ataos.

---
+++ atspectrograph/setup.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+# Source this file when starting the container to set it up
+
+echo "#"
+echo "# Loading LSST Stack"
+. /opt/lsst/software/stack/loadLSST.bash
+setup lsst_distrib
+echo "#"
+echo "# Loading sal environment"
+. repos/ts_sal/setup.env
+echo "#"
+echo "# Setting up ts_atspectrograph."
+
+setup ts_salobj -t $USER
+setup ts_atspectrograph -t $USER
+
+/bin/bash --rcfile /home/saluser/.bashrc
