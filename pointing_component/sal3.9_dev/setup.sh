fc4c8b9f9b4158680d020a952f54e33ea6fbc846
Tiago Ribeiro <tiago.astro@gmail.com>
2019-04-29 13:18:51 -0700

    Add labview container. Add new develop-env queue. Add pointing component separated for sal3.9 with rpm.

---
+++ pointing_component/sal3.9_dev/setup.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# Source this file when starting the container to set it up
+
+echo "#"
+echo "# Setting up and starting ATPtg CSC."
+echo "#"
+
+source scl_source enable devtoolset-3
+source /opt/lsst/software/stack/loadLSST.bash
+setup lsst_distrib
+source $LSST_SDK_INSTALL/setup.env
+setup ts_sal -t $USER
+
+/home/saluser/repos/ts_pointing_common/build/bin/ATPtgController
+# /bin/bash --rcfile /home/saluser/.bashrc
