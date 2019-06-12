f7b64646b8fb1a7488aecdbfa445381643d90b7c
Tiago Ribeiro <tiago.astro@gmail.com>
2019-06-12 09:43:13 -0700

    Add at_dome dockerfile. Change ataos to use ticket branch and checkout tag on config attcs Add salobj4 develop-env dockerfile Add DIMM dockerfile. Add ATPtg config file

---
+++ at_dome/setup.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+# Source this file when starting the container to set it up
+
+echo "#"
+echo "# Loading LSST Stack"
+. /opt/lsst/software/stack/loadLSST.bash
+setup lsst_distrib
+echo "#"
+echo "# Loading sal environment"
+. repos/ts_sal/setup.env
+echo "#"
+echo "# Setting up sal, salobj and scriptqueue"
+
+setup ts_xml -t current
+setup ts_sal -t current
+setup ts_salobj -t current
+setup ts_scriptqueue -t current
+setup ts_config_attcs -t current
+setup ts_ATDome -t current
+
+cd /home/saluser/repos/ts_ATDome/bin/
+
+while :
+    do
+        echo "# Starting ATDome CSC"
+        python ./run_atdome.py
+done
+
+
