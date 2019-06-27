2c0b04d91d91d1f0b1c1a860202004a5e17489e0
Tiago Ribeiro <tiago.astro@gmail.com>
2019-06-26 23:52:11 -0700

    Adds Jenkinsfile to build base image.

---
+++ develop-env/salobj_4/checkout_repo.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+if [ $# == 0 ]; then
+    echo "Give branch names to checkout."
+    exit 1
+else
+    echo "Got $# positional parameters."
+fi
+
+git fetch --all
+
+while [ "$1" != "" ]; do
+    if [[ "$1" == tags/* ]] ;
+    then
+        echo checkout $1 -b $1
+        git checkout $1 -b $1 && exit 0
+    else
+        echo checkout $1
+        git checkout $1 && exit 0
+    fi
+
+    shift
+
+done
+
+exit 1
+
