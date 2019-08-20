b7968413e574aa4c8bf27caecadf519554bd6c6b
couger01 <couger01@luther.edu>
2019-08-20 12:45:56 -0400

    Update test notebook to use current practice

---
+++ Compose/Simulators/start_jupyter_lab.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+. repos/ts_sal/setup.env
+. /opt/lsst/software/stack/loadLSST.bash
+setup lsst_distrib
+setup ts_xml -t current
+setup ts_sal -t current
+setup ts_salobj -t current
+setup ts_scriptqueue -t current
+jupyter lab --ip='*' --port 8886
