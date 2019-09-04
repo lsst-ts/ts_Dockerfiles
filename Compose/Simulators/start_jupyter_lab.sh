5b593cb3548126d9cbb3998e8e5db88345651f7d
couger01 <couger01@luther.edu>
2019-09-04 14:50:42 -0400

    Add missing setup and script queue image for ncsa teststand

--- Compose/Simulators/start_jupyter_lab.sh
+++ Compose/Simulators/start_jupyter_lab.sh
@@ -7,4 +7,6 @@ setup ts_xml -t current
 setup ts_sal -t current
 setup ts_salobj -t current
 setup ts_scriptqueue -t current
+setup ts_standardscripts -t current
+setup ts_externalscripts -t current
 jupyter lab --ip='*' --port 8886
