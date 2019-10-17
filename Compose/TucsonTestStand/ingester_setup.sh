aaddf8cc5c3c7dd21b98c22e91b5a0b664c0f3ae
Michael Reuter <mareuter@lsst.org>
2019-10-17 10:42:35 -0500

    Temporary fix for AT ingest.

--- Compose/TucsonTestStand/ingester_setup.sh
+++ Compose/TucsonTestStand/ingester_setup.sh
@@ -7,6 +7,13 @@ echo "# Loading LSST Stack"
 . /opt/lsst/software/stack/loadLSST.bash
 setup lsst_distrib
 
+echo "# Temporary Date fix for AT Ingestion"
+cd
+cd ingest/obs_lsst
+eups declare -r . -t lsst obs_lsst
+setup obs_lsst -t lsst
+scons
+
 echo "#"
 echo "# Starting ingest watcher"
 
