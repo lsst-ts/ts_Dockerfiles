40a5fea898746defcf7c9817bde3e4f2fe7aaa44
Tiago Ribeiro <tiago.astro@gmail.com>
2019-11-07 10:15:39 -0700

    Adds deployment environment image.

--- develop-env/conda/setup.sh
+++ develop-env/conda/setup.sh
@@ -3,6 +3,6 @@ source ~/miniconda3/bin/activate
 source ~/repos/ts_sal/setup.env
 
 export PATH=$PATH:/home/saluser/repos/ts_sal/bin.src
-export PYTHONPATH=$PYTHONPATH:/home/saluser/repos/ts_idl/python
+#export PYTHONPATH=$PYTHONPATH:/home/saluser/repos/ts_idl/python
 
 /bin/bash --rcfile /home/saluser/.bashrc
