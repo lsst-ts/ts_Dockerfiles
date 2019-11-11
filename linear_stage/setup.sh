4e0b402d64dbe3fff7905e6b749075c473c23de3
couger01 <couger01@luther.edu>
2019-11-11 15:20:35 -0300

    Fix paths

--- linear_stage/setup.sh
+++ linear_stage/setup.sh
@@ -1,10 +1,10 @@
 echo "#"
 echo "# Loading LSST Stack"
-./opt/lsst/software/stack/loadLSST.bash
+. /opt/lsst/software/stack/loadLSST.bash
 setup lsst_distrib
 echo "#"
 echo "# Loading sal environment"
-./repos/ts_sal/setup.env
+. ~/repos/ts_sal/setup.env
 echo "#"
 echo "# Setting up sal, salobj and scriptqueue"
 
@@ -13,4 +13,4 @@ setup ts_sal -t current
 setup ts_idl -t current
 setup ts_salobj -t current
 
-python linear_stage_csc.py /dev/ttyUSB0 1 1
+python ~/repos/ts_linearStage/scripts/linear_stage_csc.py /dev/ttyUSB0 1 1
