046286ac7861f2a119a397055459d18274038a63
Eric Coughlin <ECoughlin@lsst.org>
2020-04-06 12:53:25 -0400

    Fix static paths and add readme for electrometer

---
+++ electrometer/README.md
@@ -0,0 +1,4 @@
+# Electrometer
+
+Building the image requires the electrometer_version argument which is a git tag/branch.
+Running the image by default expects that an electrometer be plugged into /dev/ttyUSB0, otherwise change the line in the docker-compose.yaml.
--- electrometer/setup.sh
+++ electrometer/setup.sh
@@ -4,7 +4,7 @@ source /opt/lsst/software/stack/loadLSST.bash
 setup lsst_distrib
 echo "#"
 echo "# Loading sal environment"
-source ~/repos/ts_sal/setup.env
+source /home/saluser/repos/ts_sal/setup.env
 echo "#"
 echo "# Setting up sal, salobj and scriptqueue"
 
@@ -14,4 +14,4 @@ setup ts_idl -t current
 setup ts_salobj -t current
 setup ts_electrometer -t current
 
-python ~/repos/ts_electrometer2/bin/RunElectrometer.py 1
+python /home/saluser/repos/ts_electrometer2/bin/RunElectrometer.py 1
