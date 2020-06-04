13a2bdfb74a94aa769a6eec8e427ad5f9b3a7059
Tiago Ribeiro <tiago.astro@gmail.com>
2020-06-04 16:14:29 -0700

    Implement some minor bug fixes on the Jenkinsfiles to pull the images before the build. Add labels to images.

--- develop-env/salobj/setup.sh
+++ develop-env/salobj/setup.sh
@@ -2,4 +2,16 @@
 
 source ~/.setup_salobj.sh
 
+setup ts_config_atcalsys -t current
+setup ts_config_attcs -t current
+setup ts_config_eas -t current
+setup ts_config_latiss -t current
+setup ts_config_mtcalsys -t current
+setup ts_config_mttcs -t current
+setup ts_config_ocs -t current
+setup ts_idl -t current
+setup ts_sal -t current
+setup ts_salobj -t current
+setup ts_xml -t current
+
 /bin/bash --rcfile /home/saluser/.bashrc
